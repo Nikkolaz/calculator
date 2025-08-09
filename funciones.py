6ed3c6a91b68012c52e5ef10d84cfcc81e688a54
Nikkolaz <jn147860@gmail.com>
2025-08-08 19:09:07 -0500

    Mi parte lista

--- funciones.py
+++ funciones.py
@@ -1,1 +1,20 @@
 #aca van las funciones
+#vamos a hacer una calculadora
+# numero1 + numero2 = resultado
+
+numero1= int(input("Ingrese el primer numero:"))
+
+numero2= int(input("Ingrese el segundo numero:"))
+
+if opcion == 1:
+    resultado = numero1 + numero2
+    print("El resultado de la suma es:", resultado)
+
+elif opcion == 2:
+    resultado = numero1 - numero2
+    print("El resultado de la resta es:",
+         resultado)
+  
+elif opcion == 3:
+    resultado = numero1 * numero2
+    print("El resultado de la multiplicación", resultado)
--- menu.py
+++ menu.py
@@ -1,1 +1,6 @@
 #acá va el menú
+print("===== CALCULADORA =====")
+print("1. Sumar")
+print("2. Restar")
+
+
